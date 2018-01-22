05b3242923d96fa324a3f8547d4f720184ec8921
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-22 17:32:35 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 class Project
-  attr_accessor :title, :backed_projects
+  attr_accessor :title, :backed_project
 
   def initialize(title)
     @title = title
+    @backed_project = backed_project
   end
 #   When a Backer instance is initialized, it should be initialized with a @backed_projects variable set to an empty array. Instances of the Backer class should have an attr_accessor for backed projects so that projects can be added to a backer's list and so that the backer can report on the projects they back.
 # When a Project instance is initialized, it should be initialized with a @backers variable set to an empty array. Instances of the Project class should have an attr_accessor for backers so that projects can add backers to their list of backers and report on the backers who support them.
