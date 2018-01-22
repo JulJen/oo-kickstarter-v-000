291f155e035b166aaef3c0b49ca1bdb0e864f016
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-22 17:22:37 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  attr_accessor :name, :project
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @project = []
+  end
+
+  def backed_project(project)
+    @project << project
+    project.name = self
+  end
+#   When a Backer instance is initialized, it should be initialized with a @backed_projects variable set to an empty array. Instances of the Backer class should have an attr_accessor for backed projects so that projects can be added to a backer's list and so that the backer can report on the projects they back.
+# When a Project instance is initialized, it should be initialized with a @backers variable set to an empty array. Instances of the Project class should have an attr_accessor for backers so that projects can add backers to their list of backers and report on the backers who support them.
+# When a backer has added a project to its list of backed projects, that project should also add the backer to its list of backers. Refer back to the Code Along about Collaborating Objects.
+end
